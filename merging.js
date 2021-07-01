381d5b093e324a5f53d4a0a36788ad46b371d53d
Tarneka Ezell <tarnekaezell@gmail.com>
2021-06-30 21:17:43 -0500

    initial commit

---
+++ merging.js
@@ -0,0 +1,13 @@
+var clientName = 'Alice Mckinney';
+
+if (clientName == 'Alice Mckinney') {
+  alert('Hey Mama Alice!');
+} else if (clientName == 'Cara Mckinney') {
+  alert('Hey Papa Cara!');
+} else {
+  alert('How can I help you');
+}
+var clientNumber = 573;
+var clientage = 97;
+var clientId = 'ZEM5557841';
+var clientPhoneNumber = 6624891234;
